6dbbd44cdbba381b4754b4ed4de717087cacdd40
Vencator <rys@gmx.us>
2023-03-30 00:00:18 +0200

    change project structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,22 @@
 import { Team } from "./Team";
+import teamsHeaderHtml from 'bundle-text:../../templates/teams-header.html';
+import playersHeader from 'bundle-text:../../templates/players-header.html';
 
 export class App {
-  
+
   constructor() {
     this.team = new Team();
   }
 
+  static renderNoJsHtml = () => {
+    document.getElementById('box-1').innerHTML = teamsHeaderHtml;
+    document.getElementById('box-2').innerHTML = playersHeader;
+  }
+
   start() {
-    this.team.getAndRenderAllTeams();
+    App.renderNoJsHtml();
+    requestAnimationFrame(() => {
+      this.team.getAndRenderAllTeams();
+    });
   }
 }
